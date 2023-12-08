c340773c93761c48ad6f7408cf68b975fc6d6758
roshaans2 <roshaansathya@gmail.com>
2023-12-08 11:30:22 +0530

    Solved Medium Questions

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,7 +3,26 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+function isChar(c){
+  return c>='a' && c<='z';
+}
+
 function isPalindrome(str) {
+  str = str.toLowerCase();
+  let n = str.length;
+  let i = 0;
+  let j = n-1;
+  while(i<j){
+    while(i<n && !isChar(str[i])){
+      i++;
+    }
+    while(j>=0 && !isChar(str[j])){
+      j--;
+    }
+    if(i<j && str[i++] != str[j--]){
+      return false;
+    }
+  }
   return true;
 }
 
